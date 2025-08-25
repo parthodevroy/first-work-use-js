34489a3a86c59a81b400f21e7782f25475ab1b4c
unknown <you@example.com>
2025-08-25 11:35:06 -0700

    added new cash out section

--- home.js
+++ home.js
@@ -22,3 +22,31 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
 
     document.getElementById('available-balance').innerText=totalnewavailablebalance
 })
+
+document.getElementById('add-money').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display='none'
+    document.getElementById('add-money-parent').style.display='block'
+    
+    
+})
+document.getElementById('cash-out').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display='none'
+    document.getElementById('cash-out-parent').style.display='block'
+})
+// cash out oparation
+document.getElementById('cash-out-btn').addEventListener('click', function(e){
+e.preventDefault()
+const agentnumber=document.getElementById('cash-out-agent').value
+const cashoutamount= parseInt( document.getElementById('cash-witdraw').value)
+
+const cashoutpin=document.getElementById('cash-out-pin').value
+
+const availablebalance= parseInt(document.getElementById('available-balance').innerText)
+const newcashoutbalance=availablebalance-cashoutamount
+
+document.getElementById('available-balance').innerText=newcashoutbalance
+
+
+
+
+})
